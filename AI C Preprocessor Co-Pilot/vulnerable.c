931ca8a3607349803223ecceed1eef3e9dad851a
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-10 11:53:20 +0200

    Update vulnerable.c

--- AI C Preprocessor Co-Pilot/vulnerable.c
+++ AI C Preprocessor Co-Pilot/vulnerable.c
@@ -1,1 +1,12 @@
-
+list_t *add_node_end(list_t *head, const int n) {
+    list_t *new_node = malloc(sizeof(list_t));
+    list_t *current = head;
+    if (!head)
+        return (new_node);
+    while (current)
+        current = current->next;
+    current = new_node;
+    new_node->n = n;
+    new_node->next = NULL;
+    return (head);
+}
